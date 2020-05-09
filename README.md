c2da8f32daa0764fb80167c60a6d7c0fa3da29aa
Tperam <1929564872@qq.com>
2020-05-09 16:54:28 +0800

    Completing the contract

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# Crowdfunding
+# Crowdfunding DAICO众筹项目
+
+## 项目描述
+	利用 Solidity,Ethereum,以及Nodejs等技术完成一个多DAICO模型合约
+	一个在运行于eth链上的众筹平台，每一个众筹都是一个合约项目 都是一个以DAICO模型的合约
+
+### 什么是 DAICO
+	我们先来看 ICO 的风险和 DAO 的风险：
+
+	ICO ：都面临团队不负责任或者项目仅仅是一个骗局的风险
+	DAO ：任何投票系统都面临51%攻击的问题、贿赂选票和其他博弈上的缺陷。
+	DAICO 则是两者的组合：在 DAICO 中，这些风险都得到了最小化。
+
+	DAICO 合约由一个需要募集资金的开发团队发布。DAICO 合约有两种模式（贡献模式，自毁模式）由开始生效以后合约进入贡献模式，每个人都可以将以太贡献到合约当中，并得到相应的代币。当贡献阶段结束后，就无法再继续贡献以太，初始的代币余额将设定，之后代币可以被交易。在贡献阶段结束后，合约有一个主要状态变量：tap ，初始值为零。tap 决定每秒钟开发团队可以从合约中提现的数量。也就是说，团队无法从合约地址中直接全部提现。持有代币的人拥有投票权去改变这个 tap 值，投票者可以给开发团队一个合理而不太高的每月预算，假如团队不断证明其能力，预算可以通过投票提高。如果投票者对团队的开发进展不满意，他们可以完全关闭 DAICO合约，DAICO进入“自毁模式”，并取回自己的资金。
+摘取:
+	作者：牧笛2017
+	链接：https://www.jianshu.com/p/e613e7caa332
+	来源：简书
+	著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+## 项目要求
+
+> 遵循ERC20
+
+```solidity
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+	/*
+		定义接口
+	*/
+    function totalSupply() public constant returns (uint); // 总供应
+    function balanceOf(address tokenOwner) public constant returns (uint balance); // 查询地址
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining); // 
+    function transfer(address to, uint tokens) public returns (bool success); // 直接转账
+    function approve(address spender, uint tokens) public returns (bool success); // 授权
+    function transferFrom(address from, address to, uint tokens) public returns (bool success); // 从授权之后的from转账到to 
+
+    event Transfer(address indexed from, address indexed to, uint tokens); // 监听事件 当此方法被调用，会通知前端
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens); // 监听事件 当此方法被调用，会通知前端
+}
+
+```
+
+> 遵循Dao `去中心化自治组织`
+主要用于限制项目发起方的行动，限制他对众筹资金的权力
+- 如果在一定时间内没有达到总众筹资金，则直接原路返还给所有投资者
+- 当众筹完成之后，项目方可以发起投票申请取钱，每一位投资者都有投票的权力(依据投资金额)，超过50%投票则同意取钱
+- 投资者可以在特定的时间内(在项目进行到1/3时,2/3时)，决定是否对项目进行终止，当51%的投资权力认为需要停止项目。则直接按比例退回剩余资金
+
+
+### 当项目完成
+当项目完成之后,
--- contract/Crowdfunding.sol
+++ contract/Crowdfunding.sol
@@ -1,37 +1,79 @@
 pragma solidity ^0.4.26;
 
-contract ProjectList{
-    // 合约列表
-
-    address[] ProjectList public; // 记录项目名
 
 
-    // 创建一个项目
-	function createPorject(address owner, bytes32 name, uint goal, uint maxInvest, uint minInvest, string description) public {
-		address p1 = new Project(owner,name,goal,maxInvest,minInvest,description);
-		ProjectList.push(p1);
-	}
-     
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+	// 定义接口
+    function totalSupply() public constant returns (uint); // 总供应
+    function balanceOf(address tokenOwner) public constant returns (uint balance); // 查询地址
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining); // 
+    function transfer(address to, uint tokens) public returns (bool success); // 直接转账
+    function approve(address spender, uint tokens) public returns (bool success); // 授权
+    function transferFrom(address from, address to, uint tokens) public returns (bool success); // 从授权之后的from转账到to 
+
+    event Transfer(address indexed from, address indexed to, uint tokens); // 监听事件 当此方法被调用，会通知前端
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens); // 监听事件 当此方法被调用，会通知前端
 }
 
-contract Project{
+/*
+	当前平台运行
+*/
+// contract ProjectList{
+//     // 合约列表
+
+//     address[] ProjectList public; // 记录项目名
+
+//     // 创建一个项目
+// 	function createPorject(address owner, bytes32 name, uint goal, uint maxInvest, uint minInvest, string description) public {
+// 		address p1 = new Project(owner,name,goal,maxInvest,minInvest,description);
+// 		ProjectList.push(p1);
+// 	}
+     
+// }
+
+/*
+	DACIO模型
+*/
+contract Project is ERC20Interface{
 	using SafeMath for uint; // 使用安全库
 
-	address owner public; // 合约拥有者的地址
-	bytes32 name public; // 合约名称
+	address public owner ; // 合约拥有者的地址
+	bytes32 public projectName;
+	uint public goal ; // 众筹金额
+	uint public maxInvest ; // 个人最大投资金额
+	uint public minInvest ; // 个人最小投资金额
+	string public description ; // 项目描述
 
-	uint goal public; // 众筹金额
-	uint maxInvest public; // 个人最大投资金额
-	uint minInvest public; // 个人最小投资金额
-	string description public; // 项目描述
+	bytes32 symbol; // 币名简称
+	bytes32 name; // 币名全程
+	uint8 public decimals; // 小数点 bi
+    uint public _totalSupply;  // 总供应量
+    uint public startDate; // 众筹开始时间
+    uint public bonusEnds; // 奖励时间
+    uint8 public bonusPercent; // 奖励百分比 bonusPercent = 0.2
+    uint public weiToToken; // wei 转换 token价值
+    uint public endDate; // 众筹结束时间
 
 	mapping(address=>Investor) investors; // 投资者 地址对应信息
 
-	Payment[] paymentList public;
+	/*
+		allowed[tokenOwner][spender];
+		代表 tokenOwner 允许 spender 进行消费，可消费值为value设定值
+		可以通过 allowance 方法进行查询
+		可以通过 approve 方法进行设置哪个地址可以使用当前账户的特定金额
+	*/
+    mapping(address => mapping(address => uint)) allowed; // 允许使用资金
+
+
+	Payment[] public paymentList ; // 投票记录
 
 	struct Investor { // 投资者
 
-		uint amount; // 投资金额
+		uint amount; // 币
 		uint[] votingRecord; // 记录投票的id
 		// Payment.id 对应 是否支持
 		mapping(uint=>bool) voteResult;
@@ -48,39 +90,87 @@ contract Project{
 		uint amount; // 金额
 		bool completed; // 记录当前交易是否取出过 true表示已取出，false表示未取出
 		uint votingCount; // 同意的比例
-		uint endTime;
+		uint endDate;
 	}
 
 	modifier ownerOnly() { 
 		require (owner == msg.sender); 
-		_; 
+		_;
 	}
-	
-
-	// 投资
-	function contribute() payable public { // 表明当前方法是可以进行支付的
-
-		uint value = investors[msg.sender].amount.add(msg.value);
-
-		require(value<=maxInvest); // 过滤当前投资大于 maxInvest
-		require(value>=minInvest); // 过滤当前投资小于 minInvest
-		require(address(this).balance.add(msg.value) > goal); // 判断当前投资总数有没有超过众筹金额
-
-		investors[msg.sender] = value; // 记录
+	modifier completeCrowdfunding(){
+		require (now > endDate);
+		_;
 	}
+	modifier investable() { 
+		require ((now >= startDate) && (now <= endDate)); 
+		_;
+	}
+
+
+	constructor(address _owner, bytes32 _projectName, uint _goal, uint _maxInvest, uint _minInvest, string _description, bytes32 _symbol, bytes32 _name, uint8 _decimals, uint _startDate, uint _bonusEnds, uint8 _bonusPercent, uint _weiToToken, uint _endDate) public{
+		owner = _owner;
+		projectName = _projectName;
+		goal = _goal;
+		_totalSupply = 0;
+		maxInvest = _maxInvest;
+		minInvest = _minInvest;
+		description = _description;
+		symbol = _symbol;
+		name = _name;
+		decimals = _decimals;
+		startDate = _startDate;
+		bonusEnds = _bonusEnds;
+		bonusPercent = _bonusPercent;
+		weiToToken = _weiToToken;
+		endDate = _endDate;
+	}
+
+	// 实现 ERC20 接口
+
+	// 当前供应量
+	function totalSupply() public view returns (uint){
+		return _totalSupply;
+	}
+	// 查询存款
+	function balanceOf(address tokenOwner) public view returns (uint){
+		return investors[tokenOwner].amount;
+	}
+	// 查询允许使用token的账户
+	function allowance(address tokenOwner, address spender) public view returns (uint){
+		return allowed[tokenOwner][spender];
+	}
+	// 转账
+	function transfer(address to, uint tokens) public returns (bool success){
+		// from 默认为 msg.sender
+		investor _from = investors[msg.sender];
+		investor _to = investors[to];
+
+		require( _from.amount >= tokens); // 转账者的token数量必须大于转账数量
+
+		Payment[] memory ongoingVoting; // 正在进行的投票
+
+		for( uint i=0; i<paymentList.length; i ++){
+			// 如果截止时间小于现在时间，那么代表当前投票已经关闭。
+			// 如果completed等于true，那么代表当前投票已经通过（）
+			if( paymentList[i].endDate < now || completed ){ 
+
+			}
+		}
+	}
+
 
 	// 创建一次 投票取钱
-	function createPayment(string description, uint amount,uint endTime) ownerOnly public{
+	function createPayment(string _description, uint amount,uint endDate) ownerOnly public{
 		
 		// 创建一次投票
-		Payment p0 = Payment({
+		Payment memory p0 = Payment({
 			id: paymentList.length,
-			description,
-			amount,
+			description:_description,
+			amount:amount,
 			completed: false,
 			votingCount: 0,
-			endTime
-		})
+			endDate:endDate
+		});
 
 		// 添加进投票列表里
 		paymentList.push(p0);
@@ -89,16 +179,44 @@ contract Project{
 	// 投票 是否同意取钱
 	// id: payment id
 	// flag: 是否同意 true = 同意， false = 不同意.
-	function votePayment(uint id, bool flag){
-		if ( !investors[msg.sender].votingRecord[id] && flag){ // 判断是否投过票 没投过票则继续判断 这次投票是否为同意
-			investors[msg.sender]
-		} 
+	function approvePayment(uint id, bool flag) public{
+		// 寻找是否投过票
+		for(uint i=0; i < investors[msg.sender].votingRecord.length; i ++){
+	        if ( investors[msg.sender].votingRecord[i]==id ){
+	            return;
+	        }
+		}
+		investors[msg.sender].votingRecord.push(id);
+		
+		if ( flag ){
+		    investors[msg.sender].voteResult[id]=flag;
+		    paymentList[id].votingCount = paymentList[id].votingCount.add(investors[msg.sender].amount);
+		}
+		
 
 		
 	}
 
 
-	function() payable public{} // 表明当前合约是可以进行支付的
+	function () public payable {
+		require(now >= startDate && now < endDate);
+		uint tokens;
+		if (now <= bonusEnds) {
+			tokens = msg.value.mul(weiToToken.mul(bonusPercent)); // 1 wei = 1.2 token
+		} else {
+			tokens = msg.value.mul(weiToToken); // 1 wei = 1 token
+		}
+
+		uint totalToken = investors[msg.sender].amount.add(tokens);
+		require (totalToken > minInvest); // 限制个人最多购买数量
+		require (totalToken < maxInvest); // 个人最小购买数
+		investors[msg.sender].amount = totalToken;
+		_totalSupply = _totalSupply.add(tokens);
+		emit Transfer(address(0), msg.sender, tokens);
+	}
+
+	function contribute () payable {}
+	
 }
 
 
